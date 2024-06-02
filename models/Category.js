aae1cebddc40ab971c73d49fccb23433e516f504
Cameron Posey <cameronposey@outlook.com>
2024-06-02 06:16:39 -0700

    Increase description max-length

--- models/Category.js
+++ models/Category.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const CategorySchema = new Schema({
     name: {type: String, required: true, maxLength: 40},
-    description: {type: String, required: true, maxLength: 100},
+    description: {type: String, required: true, maxLength: 200},
 });
 
 module.exports = mongoose.model("Category", CategorySchema);
--- models/Item.js
+++ models/Item.js
@@ -3,7 +3,7 @@ const Schema = mongoose.Schema;
 
 const ItemSchema = new Schema({
     name: {type: String, required: true, maxLength: 100},
-    description: {type: String, required: true, maxLength: 150},
+    description: {type: String, required: true, maxLength: 300},
     category: {type: Schema.Types.ObjectId, ref: 'Category'},
     price: {
         type: Number, 
