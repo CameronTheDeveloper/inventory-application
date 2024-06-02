75a4f0c83c6facbca958a0568aa4f5785807c84b
Cameron Posey <cameronposey@outlook.com>
2024-06-02 01:37:21 -0700

    Add Item model

---
+++ models/Item.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ItemSchema = new Schema({
+    name: {type: String, required: true, maxLength: 100},
+    description: {type: String, required: true, maxLength: 150},
+    category: {type: Schema.Types.ObjectId, ref: 'Category'},
+    price: {
+        type: Number, 
+        required: true,
+        min: 0.01,
+        max: 1000000
+    }
+});
+
+module.exports = mongoose.model("Item", ItemSchema);
