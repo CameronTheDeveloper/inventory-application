8f2e3f3f7f9e91fdef87cd9b15490fe635640289
Cameron Posey <cameronposey@outlook.com>
2024-06-02 00:38:46 -0700

    Add Category model

---
+++ models/Category.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const CategorySchema = new Schema({
+    name: {type: String, required: true, maxLength: 40},
+    description: {type: String, required: true, maxLength: 100},
+});
+
+module.exports = mongoose.model("Category", CategorySchema);
