3e50bb1ea86270edbb59966dd6a6ec5e786420b7
Cameron Posey <cameronposey@outlook.com>
2024-06-03 01:19:03 -0700

    Add itemController skeleton code

---
+++ controllers/itemController.js
@@ -0,0 +1,34 @@
+const Item = require('../models/item');
+const asyncHandler = require('express-async-handler');
+
+exports.item_list = asyncHandler(async (req, res, next) => {
+    res.send('item_list not implemented');
+});
+
+exports.item_detail = asyncHandler(async (req, res, next) => {
+    res.send('item_detail not implemented');
+});
+
+exports.item_create_get = asyncHandler(async (req, res, next) => {
+    res.send('item_create_get not iplemented');
+});
+
+exports.item_create_post = asyncHandler(async (req, res, next) => {
+    res.send('item_create_post not iplemented');
+});
+
+exports.item_delete_get = asyncHandler(async (req, res, next) => {
+    res.send('item_delete_get not iplemented');
+});
+
+exports.item_delete_post = asyncHandler(async (req, res, next) => {
+    res.send('item_delete_post not iplemented');
+});
+
+exports.item_update_get = asyncHandler(async (req, res, next) => {
+    res.send('item_update_get not iplemented');
+});
+
+exports.item_update_post = asyncHandler(async (req, res, next) => {
+    res.send('item_update_post not iplemented');
+});
