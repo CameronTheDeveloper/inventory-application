c2f5dcef3ccf3506c1b013b0bc97494d16828858
Cameron Posey <cameronposey@outlook.com>
2024-06-03 01:15:34 -0700

    Add categoryController skeleton code

---
+++ controllers/categoryController.js
@@ -0,0 +1,34 @@
+const Category = require('../models/category');
+const asyncHandler = require('express-async-handler');
+
+exports.category_list = asyncHandler(async (req, res, next) => {
+    res.send('category_list not implemented');
+});
+
+exports.category_items = asyncHandler(async (req, res, next) => {
+    res.send('category_items not implemented');
+});
+
+exports.category_create_get = asyncHandler(async (req, res, next) => {
+    res.send('category_create_get not iplemented');
+});
+
+exports.category_create_post = asyncHandler(async (req, res, next) => {
+    res.send('category_create_post not iplemented');
+});
+
+exports.category_delete_get = asyncHandler(async (req, res, next) => {
+    res.send('category_delete_get not iplemented');
+});
+
+exports.category_delete_post = asyncHandler(async (req, res, next) => {
+    res.send('category_delete_post not iplemented');
+});
+
+exports.category_update_get = asyncHandler(async (req, res, next) => {
+    res.send('category_update_get not iplemented');
+});
+
+exports.category_update_post = asyncHandler(async (req, res, next) => {
+    res.send('category_update_post not iplemented');
+});
