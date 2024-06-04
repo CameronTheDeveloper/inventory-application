db7431d72a11913d88913612226c634ee1587537
Cameron Posey <cameronposey@outlook.com>
2024-06-04 09:56:31 -0700

    Add category_form contoller get logic

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -27,7 +27,7 @@ exports.category_items = asyncHandler(async (req, res, next) => {
 });
 
 exports.category_create_get = asyncHandler(async (req, res, next) => {
-    res.send('category_create_get not iplemented');
+    res.render('category_form', {title: 'Add a new Category'});
 });
 
 exports.category_create_post = asyncHandler(async (req, res, next) => {
