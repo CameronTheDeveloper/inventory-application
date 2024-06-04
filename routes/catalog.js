c3c683f7b222a441991108a9861f016ff963471c
Cameron Posey <cameronposey@outlook.com>
2024-06-04 13:54:21 -0700

    Fix route order in catalog.js

--- routes/catalog.js
+++ routes/catalog.js
@@ -10,8 +10,6 @@ router.get('/', category_controller.index);
 
 router.get('/categories', category_controller.category_list);
 
-router.get('/category/:id', category_controller.category_items);
-
 router.get('/category/create', category_controller.category_create_get);
 router.post('/category/create', category_controller.category_create_post);
 
@@ -21,10 +19,9 @@ router.post('/category/:id/delete', category_controller.category_delete_post);
 router.get('/category/:id/update', category_controller.category_update_get);
 router.post('/category/:id/update', category_controller.category_update_post);
 
+router.get('/category/:id', category_controller.category_items);
 
 /* Item */
-
-router.get('/item/:id', item_controller.item_detail);
 
 router.get('/item/create', item_controller.item_create_get);
 router.post('/item/create', item_controller.item_create_post);
@@ -35,5 +32,6 @@ router.post('/item/:id/delete', item_controller.item_delete_post);
 router.get('/item/:id/update', item_controller.item_update_get);
 router.post('/item/:id/update', item_controller.item_update_post);
 
+router.get('/item/:id', item_controller.item_detail);
 
 module.exports = router;
