9e08803eb60ab7b7d54258af3a25f6cf534dce74
Cameron Posey <cameronposey@outlook.com>
2024-06-06 15:26:26 -0700

    Implement compression

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const logger = require('morgan');
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const catalogRouter = require('./routes/catalog');
+const compression = require('compression');
 
 const app = express();
 
@@ -29,6 +30,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+app.use(compression());
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
