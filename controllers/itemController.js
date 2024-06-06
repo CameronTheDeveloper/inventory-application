2e12eb7e004b76d3ec186ed1c0c792642b362c1b
Cameron Posey <cameronposey@outlook.com>
2024-06-06 14:25:39 -0700

    Add item_update POST logic

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -108,6 +108,54 @@ exports.item_update_get = asyncHandler(async (req, res, next) => {
     });
 });
 
-exports.item_update_post = asyncHandler(async (req, res, next) => {
-    res.send('item_update_post not iplemented');
-});
+exports.item_update_post = [
+    body('itemName')
+        .trim()
+        .isLength({min: 2})
+        .escape()
+        .withMessage('Item name must be at least 2 characters long')
+        .matches(/^[a-zA-Z0-9 ]+$/)
+        .withMessage('Item name has non-alphanumeric characters.'),
+    body('itemDesc')
+        .trim()
+        .isLength({min: 5})
+        .escape()
+        .withMessage('Item description must be at least 5 characters long')
+        .matches(/^[a-zA-Z0-9 ]+$/)
+        .withMessage('Item description has non-alphanumeric characters.'),
+    body('itemPrice')
+        .isFloat({min: 0})
+        .withMessage('Item price must be a positive decimal number'),
+    body('itemAmountInStock')
+        .isInt({min: 0})
+        .withMessage('Item amount-in-stock must be a positive integer'),
+
+    
+    asyncHandler(async (req, res, next) => {
+        const errors = validationResult(req);
+
+        const item = new Item({
+            name: req.body.itemName,
+            description: req.body.itemDesc,
+            category: req.body.itemCategory,
+            price: req.body.itemPrice,
+            amount_in_stock: req.body.itemAmountInStock,
+            _id: req.params.id
+        });
+
+        if (!errors.isEmpty()){
+            const categories = await Category.find().exec();
+
+            res.render('item_form', {
+                title: 'Add item',
+                item: item,
+                category_list: categories,
+                errors: errors.array()
+            });
+            return;
+        } else {
+            const updatedItem = await Item.findByIdAndUpdate(req.params.id, item, {});
+            res.redirect(updatedItem.url)
+        }
+    }
+)];
